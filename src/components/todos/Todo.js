4d2af2e28d97d0fd749e8c232ebbbbf661dfabf3
pablitar <pablitar@gmail.com>
2019-05-26 22:03:02 -0300

    Agregada una clase a los todos done

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -15,6 +15,7 @@ function getIcon(todo) {
 export default function Todo({onClick,todo}) {
   const classes = ["todo"]
   if(todo.requestId) classes.push("loading")
+  if(todo.done) classes.push("done")
   return <div className={classes.join(" ")} onClick={onClick}>
     <span className = "icon">{getIcon(todo)}</span><span style={getStyleFor(todo)} className="text">{todo.description}</span>
   </div>
